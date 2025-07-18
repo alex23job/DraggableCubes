48df09a12f0fe36439d3b4b08ed31a76c78ed1a8
alex23job <106354657+alex23job@users.noreply.github.com>
2025-07-19 00:37:58 +0300

    First commit

---
+++ ProbaDraggable/Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+//using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class DraggableObject : MonoBehaviour
+{
+    //private PointerEventData pointerEventData;
+    [SerializeField] private float moveSpeed = 10f;
+    private Vector3 initialOffset;
+    private bool isDragging = false;
+
+    private void OnEnable()
+    {
+        // Подписываемся на события перетаскивания
+        InputActionAsset inputActions = Resources.Load<InputActionAsset>("InputSystem_Actions");
+        print($"inputActions=<{inputActions}>");
+        inputActions.FindActionMap("Dragging").FindAction("BeginDrag").performed += BeginDrag;
+        inputActions.FindActionMap("Dragging").FindAction("Dragging").performed += ContinueDrag;
+        inputActions.FindActionMap("Dragging").FindAction("EndDrag").performed += EndDrag;
+        inputActions.FindActionMap("Dragging").FindAction("EndDrag").canceled += EndDrag;
+    }
+
+    private void OnDisable()
+    {
+        // Отписываемся от событий
+        InputActionAsset inputActions = Resources.Load<InputActionAsset>("InputSystem_Actions");
+        inputActions.FindActionMap("Dragging").FindAction("BeginDrag").performed -= BeginDrag;
+        inputActions.FindActionMap("Dragging").FindAction("Dragging").performed -= ContinueDrag;
+        inputActions.FindActionMap("Dragging").FindAction("EndDrag").performed -= EndDrag;
+        inputActions.FindActionMap("Dragging").FindAction("EndDrag").canceled -= EndDrag;
+    }
+
+    public void BeginDrag(InputAction.CallbackContext context)
+    {
+        //print($"BeginDrag context={context}");
+        // Находим ближайший объект под курсором
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //print($"hit=<<{hit.collider} {hit.collider.name}>>");
+            if (hit.transform == transform)
+            {                
+                isDragging = true;
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+                initialOffset = transform.position - mousePos;
+                print($"initialOffset=<{initialOffset}> hit=<<{hit.collider} {hit.collider.name}>>");
+            }
+        }
+    }
+
+    public void ContinueDrag(InputAction.CallbackContext context)
+    {
+        if (isDragging)
+        {
+            // Обновляем позицию объекта в зависимости от позиции мыши
+            Vector2 look = context.ReadValue<Vector2>();
+            print($"ContinueDrag look={look} context={context}");
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            //Vector3 newPosition = mousePos + initialOffset;
+            Vector3 newPosition = transform.position;
+            look *= Time.deltaTime * moveSpeed;
+            newPosition.x += look.x;
+            newPosition.y += look.y * 2;
+            //print($"isDragging => mousePos={mousePos}   pos={transform.position}  newPos={newPosition}  initialOffset={initialOffset}");
+            transform.position = newPosition;
+        }
+    }
+
+    public void EndDrag(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            isDragging = false;
+            print($"EndDrag  context={context}");
+        }
+    }
+}
